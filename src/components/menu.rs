36b14712eaeeaae99c5e896d2bf0a31002457c0c
Rich Wandell <richwandell@gmail.com>
2023-01-28 17:43:06 -0500

    first

--- src/components/menu.rs
+++ src/components/menu.rs
@@ -1,16 +1,29 @@
+use log::info;
+use wasm_bindgen::JsValue;
 use material_yew::*;
 use yew::prelude::*;
 
 #[function_component]
 pub(crate) fn Menu() -> Html {
+    let selected_item = use_state(|| 0);
+
+    let onaction = {
+        let selected_item = selected_item.clone();
+        move |x| {
+            info!("selected index: {:?}", x);
+            let value = *selected_item + 1;
+            selected_item.set(value);
+        }
+    };
+
     html! {
         <section>
             {"This is in the drawer"}
-            <MatList>
-                <MatListItem>{"Item 0"}</MatListItem>
-                <MatListItem>{"Item 1"}</MatListItem>
-                <MatListItem>{"Item 2"}</MatListItem>
-                <MatListItem>{"Item 3"}</MatListItem>
+            <MatList onaction={onaction} >
+                <MatListItem >{"Item 0"}</MatListItem>
+                <MatListItem >{"Item 1"}</MatListItem>
+                <MatListItem >{"Item 2"}</MatListItem>
+                // <MatListItem selected={item == 3}>{"Item 3"}</MatListItem>
             </MatList>
             <MatButton label="Click me!" icon={AttrValue::from("code")} />
         </section>
